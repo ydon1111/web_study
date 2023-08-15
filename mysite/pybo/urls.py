f55959d4b2a626efa213b42a2645061644d8861a
yeongdon <kyd5022@naver.com>
2023-08-16 08:27:30 +0900

    자료정리_230816

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 # app의 url 관리 
 urlpatterns = [ 
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('<int:question_id>/',views.detail,name='detail'),
 
 ]
