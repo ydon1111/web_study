9f6bdfe413589414ff15642173a56e6943bd9bd2
yeongdon <kyd5022@naver.com>
2023-09-19 18:14:02 +0900

    login,out 구현

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'common.apps.CommonConfig',  
     "pybo.apps.PyboConfig",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -119,8 +120,14 @@ STATICFILES_DIRS = [
 ]
 
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# 로그인 성공후 이동하는 URL
+LOGIN_REDIRECT_URL = '/'
+
+# 로그아웃시 이동하는 URL
+LOGOUT_REDIRECT_URL = '/'
--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
+    path('common/',include('common.urls')),
+    path('',views.index, name ='index'),
 ]
