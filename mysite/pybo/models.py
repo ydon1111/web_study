02a87e4ff7a91d6318de5f977c4d4c00d57c7f12
yeongdon <kyd5022@naver.com>
2023-09-20 18:18:06 +0900

    추천기능 추가

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -4,12 +4,13 @@ from django.contrib.auth.models import User
 
 # db속성 설정 
 class Question(models.Model):
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    author = models.ForeignKey(User,on_delete=models.CASCADE , related_name='author_question')
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null = True, blank= True)
-    
+    voter = models.ManyToManyField(User, related_name='voter_question')  # 추천인 추가
+
     def __str__(self):
         return self.subject
 
@@ -19,4 +20,4 @@ class Answer(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True,blank=True)
-
+    voter = models.ManyToManyField(User, related_name='voter_answer')
